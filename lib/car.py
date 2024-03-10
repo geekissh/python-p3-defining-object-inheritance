e29848a577a739e27203c3d13aa757c68b40827c
Mark Kelsall <markkelsall1995@gmail.com>
2024-03-10 22:29:29 +0300

    final

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number, year, color):
+        super().__init__(wheel_size, wheel_number, year)
+        self.color = color
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    def fill_up_tank(self):
+        return "filling up!"
